b74594924f87fd8dbe13ff6ce83429f23e7e7b23
bukakage <100402999+bukakage@users.noreply.github.com>
2023-12-08 12:58:53 +0800

    to-do list keyword command

--- Project/To do app/index.js
+++ Project/To do app/index.js
@@ -1,35 +1,42 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
-function addTask(){
-    if(inputBox.value === ''){
-        alert("Task is empty");
-    } 
-    else {
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "x";
-        li.appendChild(span);
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Task is empty");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "x";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveData();
+}
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
     }
-    inputBox.value = "";
     saveData();
-}
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    } 
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        
-    }
-    saveData()
-}, false);
+  },
+  false
+);
 
-function saveData(){
-    localStorage.setItem("toDoSavedData", listContainer.innerHTML);
+inputBox.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+function saveData() {
+  localStorage.setItem("toDoSavedData", listContainer.innerHTML);
 }
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("toDoSavedData");
+function showTask() {
+  listContainer.innerHTML = localStorage.getItem("toDoSavedData");
 }
 showTask();
